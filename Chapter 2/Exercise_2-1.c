91980e584a09157fa1ccdb493a6d14914d0366b7
Northim <79011925+NorthimOvO@users.noreply.github.com>
2023-04-30 17:18:08 +0800

    Update and rename Practice_2-1.c to Exercise_2-1.c

---
+++ Chapter 2/Exercise_2-1.c
@@ -0,0 +1,1 @@
+/* Exercise 2-1: Write a program to determine the range of char, short, int, long variables, both signed and unsigned, by printing appropriate values from standard headers and by direct computation. Harder if you compute them: determine the range of the  various floating-point types. */
